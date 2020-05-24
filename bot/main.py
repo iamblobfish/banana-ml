3554a82e6ba7a64f067f7cb44aeded67c4c3fab7
iamblobfish <51192701+iamblobfish@users.noreply.github.com>
2020-05-24 12:47:38 +0300

    Add files via upload

--- bot/main.py
+++ bot/main.py
@@ -17,6 +17,7 @@ import logging
 
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from predict_model import Predictor
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -24,6 +25,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
+predictor = Predictor("trained_model_dict")
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
@@ -45,6 +47,33 @@ def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
+def action(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Здесь будет экшн')
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+def get_photo(update, context):
+    """Echo the user message."""
+    user = update.message.from_user
+    # get photo file
+    photo_file = update.message.photo[-1].get_file()
+    # save photo
+    photo_file.download('user_photo.jpg')
+    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
+    update.message.reply_text('Gorgeous! Got your photo')
+    
+    # load saved photo
+    new_photo = open('user_photo.jpg', 'rb')
+
+    # send photo
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=new_photo)
+    result = predictor.get_image_predict('user_photo.jpg')
+    context.bot.send_message(chat_id=update.effective_chat.id, text=result)
 
 def main():
     """Start the bot."""
@@ -56,10 +85,13 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-
+    dp.add_handler(CommandHandler("action", action))
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(MessageHandler(Filters.photo, get_photo))
+
+    # on noncommand i.e message - echo the message on Telegram
     # log all errors
     dp.add_error_handler(error)
 
