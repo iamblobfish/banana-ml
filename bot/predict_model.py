724442d83d8a0433b56e57297d28c0577a64e0ae
iamblobfish <51192701+iamblobfish@users.noreply.github.com>
2020-05-28 04:44:02 +0300

    Update predict_model.py

--- bot/predict_model.py
+++ bot/predict_model.py
@@ -1,4 +1,3 @@
-import os
 from PIL import Image
 
 import torch
@@ -6,81 +5,124 @@ import numpy as np
 import torchvision
 import torch.nn as nn
 
-CLASS_NAMES = {0: 'Бумага', 1: 'Камень', 2: 'Ножницы'}
+!rm Practical_DL* -rf && wget https://github.com/yandexdataschool/Practical_DL/archive/spring20.zip && unzip spring20.zip && mv */seminar06-style-transfer/images .
+style_img = image_loader("images/wave.jpg").type(dtype)
 
-class Flatten(nn.Module):
+class ContentLoss(nn.Module):
+
+    def __init__(self, target, weight):
+        super(ContentLoss, self).__init__()
+        # we 'detach' the target content from the tree used
+        self.target = target.detach() * weight
+        self.weight = weight
+
     def forward(self, input):
-        return input.view(input.size(0), -1)
+        self.loss = F.mse_loss(input * self.weight, self.target)
+        return input.clone()
 
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, activation=nn.ReLU()):
-        super().__init__()
+    def backward(self, retain_graph=True):
+        self.loss.backward(retain_graph=retain_graph)
+        return self.loss
 
-        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
-                              kernel_size=kernel_size, stride=1, padding=1)
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.activation = activation
+def gram_matrix(input):
+    a, b, c, d = input.size()  # a=batch size(=1)
+    # b=number of feature maps
+    # (c,d)=dimensions of a f. map (N=c*d)
 
-        self.conv2 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size,
-                               stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(out_channels)
+    features = input.view(a * b, c * d)  # resise F_XL into \hat F_XL
 
-    def forward(self, inputs):
-        x = self.conv(inputs)
-        x = self.bn(x)
-        x = self.activation(x)
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.activation(x + inputs)
-        return x
+    G = torch.mm(features, features.t())  # compute the gram product
+
+    # we 'normalize' the values of the gram matrix
+    # by dividing by the number of element in each feature maps.
+    return G.div(a * b * c * d)
     
-class Block(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, activation=nn.ReLU(), padding=1):
-        super().__init__()
+class StyleLoss(nn.Module):
 
-        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
-                              kernel_size=kernel_size, stride=stride, padding=padding)
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.activation = activation
+    def __init__(self, target, weight):
+        super(StyleLoss, self).__init__()
+        self.target = target.detach() * weight
+        self.weight = weight
 
-    def forward(self, inputs):
-        x = self.conv(inputs)
-        x = self.bn(x)
-        x = self.activation(x)
-        return x
-    
-    
+    def forward(self, input):
+        self.G = gram_matrix(input)
+        self.G.mul_(self.weight)
+        self.loss = F.mse_loss(self.G, self.target)
+        return input.clone()
+
+    def backward(self, retain_graph=True):
+        self.loss.backward(retain_graph=retain_graph)
+        return self.loss
+
 
 class Predictor:
     def __init__(self, path='trained_model_dict'):
-        self.model = self.model = nn.Sequential(Block(in_channels=3, out_channels=48, kernel_size=3, stride=2),
-
-                                ResidualBlock(in_channels=48, out_channels=48, kernel_size=3),
-                                nn.MaxPool2d(kernel_size=2, stride=2),
-
-                                Block(in_channels=48, out_channels=96, kernel_size=3, stride=1),
-                                nn.MaxPool2d(kernel_size=2, stride=2),
-
-                                ResidualBlock(in_channels=96, out_channels=96, kernel_size=3),
-                                nn.MaxPool2d(kernel_size=2, stride=2),
-
-                                Block(in_channels=96, out_channels=192, kernel_size=3, stride=2),
-                                nn.MaxPool2d(kernel_size=2, stride=2),
-
-                                ResidualBlock(in_channels=192, out_channels=192, kernel_size=3),
-                                nn.MaxPool2d(kernel_size=2, stride=2),
-
-                                Flatten(),
-                                nn.Linear(768, 3),
-                                nn.LogSoftmax())
+        self.model = self.model = nn.Sequential(conv_1= Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)), 
+                                                style_loss_1= StyleLoss(), 
+                                                relu_1= ReLU(inplace=True), 
+                                                conv_2= Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+                                                style_loss_2= StyleLoss(),
+                                                relu_2= ReLU(inplace=True),
+                                                pool_3= MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+                                                conv_3= Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+                                                style_loss_3= StyleLoss(),
+                                                relu_3= ReLU(inplace=True),
+                                                conv_4= Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+                                                content_loss_4= ContentLoss(),
+                                                style_loss_4= StyleLoss(),
+                                                relu_4= ReLU(inplace=True),
+                                                pool_5= MaxPool2d(kernel_si,ze=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+                                                conv_5= Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+                                                style_loss_5= StyleLoss(),
+                                                relu_5= ReLU(inplace=True),
+                                                conv_6= Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+                                                relu_6= ReLU(inplace=True),
+                                                conv_7= Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+                                                relu_7= ReLU(inplace=True),
+                                                conv_8= Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+                                                relu_8= ReLU(inplace=True),
+                                                pool_9= MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+                                                conv_9= Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+                                                relu_9= ReLU(inplace=True),
+                                                conv_10= Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+                                                relu_10= ReLU(inplace=True),
+                                                conv_11= Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+                                                relu_11= ReLU(inplace=True),
+                                                conv_12= Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+                                                relu_12= ReLU(inplace=True),
+                                                pool_13= MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+                                                conv_13= Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+                                                relu_13= ReLU(inplace=True),
+                                                conv_14= Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+                                                relu_14= ReLU(inplace=True),
+                                                conv_15= Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+                                                relu_15= ReLU(inplace=True),
+                                                conv_16= Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+                                                relu_16= ReLU(inplace=True),
+                                                pool_17= MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)))
 
         self.model.load_state_dict(torch.load(path))
 
     def get_image_predict(self, img_path='img_path.jpg'):
+        style_losses = [StyleLoss(), StyleLoss(), StyleLoss(), StyleLoss(), StyleLoss()]
+        optimizer = torch.optim.LBFGS([input_image])
         image = Image.open(img_path).convert('RGB').resize((300, 300), Image.ANTIALIAS)
         img_tensor = torch.tensor(np.transpose(np.array(image), (2, 0, 1))).unsqueeze(0)
         print('Before normalize', torch.max(img_tensor))
-        img_tensor = img_tensor / 255.
-        class_index = np.argmax(self.model(img_tensor).data.numpy()[0])
-        result = CLASS_NAMES[class_index]
-        return result
+        input_image = img_tensor / 255.
+        for i in range(300):
+          # correct the values of updated input image
+          input_image.data.clamp_(0, 1)
+          model(input_image)
+          style_score = 0
+          content_score = 0
+          for sl in style_losses:
+              style_score += sl.backward()
+          for cl in content_losses:
+              content_score += cl.backward()
+
+          loss = style_score + content_score
+          
+          optimizer.step(lambda:loss)
+          optimizer.zero_grad()
+        return input_image.cpu().data.numpy()[0].transpose(1, 2, 0)
